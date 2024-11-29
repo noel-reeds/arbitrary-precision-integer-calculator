0dc9356fcd21e5f796d7c476aafeec2a15396f2f
altoid <noelreeds01@gmail.com>
2024-11-29 12:03:47 +0000

    api-calc entry point

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "arbitrary.h"
+
+/**
+ * main - entry point of our api-calculator.
+ *
+ *@argc: arguments count.
+ *
+ *@argv: arguments vector.
+ *
+ * Return: an integer from the integer operation.
+ */
+
+int main(int argc, char **argv) {
+	return (0);
+}
