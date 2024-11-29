9437eca4483c7c5e16e910c1776e5113bc5622ab
altoid <noelreeds01@gmail.com>
2024-11-29 14:17:07 +0000

    added macros

--- arbitrary.h
+++ arbitrary.h
@@ -1,9 +1,17 @@
 #ifndef ARBITRARY_H
 #define ARBITRARY_H
 
-/*--- HEADER FILES ---*/
+#define FALSE 0
+#define TRUE 1
+
+/*--- header files ---*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+/*--- custom function defs ---*/
+int main(int argc, char **argv);
+void add(int num, int num3);
 
+#endif
