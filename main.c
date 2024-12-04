aaa057845a0533ab05f1a2dc770a85ab74459faf
altoid <noelreeds01@gmail.com>
2024-12-04 14:08:00 +0000

    basic add for more than two integers

--- main.c
+++ main.c
@@ -10,63 +10,56 @@
  * Return: an integer from the integer operation.
  */
 
-int main(int argc, char **argv) {
+uint64_t main(uint64_t argc, char **argv) {
 	int m = 1;
+	uint64_t num, *arr[argc-2];
 	uint64_t res, first_num, second_num;
 
-	while (TRUE) {
-		/* if program name is supplied. */
-		if (argc <= 3) {
-			fprintf(stderr, "Usage: <arithmetic operation> num1 num2.\n");
-			break;
+	if (argc <= 3) {
+		fprintf(stderr, "Usage: <arithmetic operation> num1 num2.\n");
+		break;
+	}
+	if (strcmp(argv[m], "add") == 0) {
+		/* check arguments to add func have been
+		* passed to the cmd */
+		for (m = 2; m < argc; m++) {
+			res = 0;
+			arr[m] = strtoul(argv[m], NULL, 10);
 		}
-		if (strcmp(argv[m], "add") == 0) {
-			/* check arguments to add func have been
-			* passed to the cmd */
-			if (argv[m+1] != NULL && argv[m + 2] != NULL) {
-				first_num = strtoul(argv[m + 1], NULL, 10);
-				second_num = strtoul(argv[m + 2], NULL, 10);
+		res = arbitrary_add(argc, *arr[]);
+		printf("sum: %lu\n", res);
+		break;
+	}
+	else if (strcmp(argv[m], "sub") == 0) {
+		first_num = strtoul(argv[m + 1], NULL, 10);
+		second_num = strtoul(argv[m + 2], NULL, 10);
 
-				res = arbitrary_add(first_num, second_num);
-				printf("sum: %lu\n", res);
-				break;
-			}
-			else {
-				printf("usage: add num1 num2.\n");
-			}
-		}
-		else if (strcmp(argv[m], "sub") == 0) {
-			first_num = strtoul(argv[m + 1], NULL, 10);
-			second_num = strtoul(argv[m + 2], NULL, 10);
+		res = arbitrary_sub(first_num, second_num);
+		printf("sub: %lu\n", res);
+		break;
+	}
+	else if (strcmp(argv[m], "mul") == 0) {
+		first_num = strtoul(argv[m + 1], NULL, 10);
+		second_num = strtoul(argv[m + 2], NULL, 10);
 
-			res = arbitrary_sub(first_num, second_num);
-			printf("sub: %lu\n", res);
-			break;
-		}
-		else if (strcmp(argv[m], "mul") == 0) {
-			first_num = strtoul(argv[m + 1], NULL, 10);
-			second_num = strtoul(argv[m + 2], NULL, 10);
+		res = arbitrary_mul(first_num, second_num);
+		printf("mul: %lu\n", res);
+		break;
+	}
+	else if (strcmp(argv[m], "div") == 0) {
+		first_num = strtoul(argv[m + 1], NULL, 10);
+		second_num = strtoul(argv[m + 2], NULL, 10);
 
-			res = arbitrary_mul(first_num, second_num);
-			printf("mul: %lu\n", res);
-			break;
-		}
-		else if (strcmp(argv[m], "div") == 0) {
-			first_num = strtoul(argv[m + 1], NULL, 10);
-			second_num = strtoul(argv[m + 2], NULL, 10);
+		res = arbitrary_div(first_num, second_num);
+		printf("div: %lu\n", res);
+		break;
+	}
+	else if (strcmp(argv[m], "mod") == 0) {
+		first_num = strtoul(argv[m + 1], NULL, 10);
+		second_num = strtoul(argv[m + 2], NULL, 10);
 
-			res = arbitrary_div(first_num, second_num);
-			printf("div: %lu\n", res);
-			break;
-		}
-		else if (strcmp(argv[m], "mod") == 0) {
-			first_num = strtoul(argv[m + 1], NULL, 10);
-			second_num = strtoul(argv[m + 2], NULL, 10);
-
-			res = arbitrary_mod(first_num, second_num);
-			printf("modulo: %lu\n", res);
-			break;
-		}
-	}	
+		res = arbitrary_mod(first_num, second_num);
+		printf("modulo: %lu\n", res);
+		break;
+	}
 	return (0);
-}
