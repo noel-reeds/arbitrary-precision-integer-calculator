7eace8ae070f52c2465b8d7ac5e6debd4a1e2249
altoid <noelreeds01@gmail.com>
2024-12-04 14:06:09 +0000

    basic add for more than two integers

--- add.c
+++ add.c
@@ -1,13 +1,24 @@
 #include "arbitrary.h"
 
 /**
- * add - adds two integers
+ * add - adds two or more integers
  * 
  * Return: returns the sum of two integers..
  */
-uint64_t arbitrary_add(uint64_t num, uint64_t num3) {
+uint64_t arbitrary_add(uint64_t argc, uint64_t *arr[]) {
+	int m;
 	uint64_t sum;
-
-	sum = num + num3;
-	return sum;
+	
+	if (argc == 4) {
+		sum = num + num3;
+		return sum;
+	}
+	else if (argc > 4) {
+		sum = 0;
+		for (m = 2; m < argc; m++) {
+			sum =+ argv[m];
+		}
+		return sum;
+	}
+	return (0);
 }
