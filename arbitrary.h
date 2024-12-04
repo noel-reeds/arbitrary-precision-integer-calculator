b9e625eadc4e75945dc17e08f29e447d3d195da6
altoid <noelreeds01@gmail.com>
2024-12-04 08:20:19 +0000

    specifies sizeof int across platforms

--- arbitrary.h
+++ arbitrary.h
@@ -3,16 +3,19 @@
 
 #define FALSE 0
 #define TRUE 1
+#define ULL unsigned long long
 
 /*--- header files ---*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 /*--- custom function defs ---*/
 int main(int argc, char **argv);
 char func_handler(char func_name);
-int arbitrary_add(int num, int num3);
+UL int arbitrary_add(unsigned long int num, unsigned long int num3);
+int arbitrary_sub(int num, int num3);
 
 #endif
