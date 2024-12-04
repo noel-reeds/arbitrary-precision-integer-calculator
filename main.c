8b1476580f0047ec4e2fc27861a9bd5ea5a4dc3d
altoid <noelreeds01@gmail.com>
2024-12-04 14:49:53 +0000

    refactors

--- main.c
+++ main.c
@@ -10,56 +10,42 @@
  * Return: an integer from the integer operation.
  */
 
-uint64_t main(uint64_t argc, char **argv) {
+int main(int argc, char **argv) {
 	int m = 1;
-	uint64_t num, *arr[argc-2];
-	uint64_t res, first_num, second_num;
+	uint64_t num, res, num3, *arr[argc-2];
 
 	if (argc <= 3) {
 		fprintf(stderr, "Usage: <arithmetic operation> num1 num2.\n");
-		break;
+		return (0);
 	}
 	if (strcmp(argv[m], "add") == 0) {
 		/* check arguments to add func have been
 		* passed to the cmd */
 		for (m = 2; m < argc; m++) {
 			res = 0;
-			arr[m] = strtoul(argv[m], NULL, 10);
+			*arr[m] = strtoul(argv[m], NULL, 10);
 		}
 		res = arbitrary_add(argc, *arr[]);
 		printf("sum: %lu\n", res);
-		break;
 	}
-	else if (strcmp(argv[m], "sub") == 0) {
-		first_num = strtoul(argv[m + 1], NULL, 10);
-		second_num = strtoul(argv[m + 2], NULL, 10);
+	num = strtoul(argv[m + 1], NULL, 10);
+	num3 = strtoul(argv[m + 2], NULL, 10);
 
-		res = arbitrary_sub(first_num, second_num);
+	if (strcmp(argv[m], "sub") == 0) {
+		res = arbitrary_sub(num, num3);
 		printf("sub: %lu\n", res);
-		break;
 	}
 	else if (strcmp(argv[m], "mul") == 0) {
-		first_num = strtoul(argv[m + 1], NULL, 10);
-		second_num = strtoul(argv[m + 2], NULL, 10);
-
-		res = arbitrary_mul(first_num, second_num);
+		res = arbitrary_mul(num, num3);
 		printf("mul: %lu\n", res);
-		break;
 	}
 	else if (strcmp(argv[m], "div") == 0) {
-		first_num = strtoul(argv[m + 1], NULL, 10);
-		second_num = strtoul(argv[m + 2], NULL, 10);
-
-		res = arbitrary_div(first_num, second_num);
+		res = arbitrary_div(num, num3);
 		printf("div: %lu\n", res);
-		break;
 	}
 	else if (strcmp(argv[m], "mod") == 0) {
-		first_num = strtoul(argv[m + 1], NULL, 10);
-		second_num = strtoul(argv[m + 2], NULL, 10);
-
-		res = arbitrary_mod(first_num, second_num);
+		res = arbitrary_mod(num, num3);
 		printf("modulo: %lu\n", res);
-		break;
 	}
 	return (0);
+}
