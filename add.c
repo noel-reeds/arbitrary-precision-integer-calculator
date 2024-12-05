6fe57c76027a2fc743fc646c3012b327b92e9b5a
altoid <noelreeds01@gmail.com>
2024-12-05 21:02:20 +0000

    passes correct argc to add

--- add.c
+++ add.c
@@ -5,19 +5,13 @@
  * 
  * Return: returns the sum of two integers..
  */
-uint64_t arbitrary_add(uint64_t argc, uint64_t *arr[]) {
-	uint64_t m, sum;
-	
-	if (argc == 4) {
-		sum = *arr[2] + *arr[3];
-		return sum;
+uint64_t arbitrary_add(int argc, uint64_t *arr[]) {
+	int m;
+	uint64_t sum;
+
+	sum = 0;
+	for (m = 0; m < argc; m++) {
+		sum = sum + *arr[m];
 	}
-	else if (argc > 4) {
-		sum = 0;
-		for (m = 2; m < argc; m++) {
-			sum = sum + *arr[m];
-		}
-		return sum;
-	}
-	return (0);
+	return sum;
 }
