6d7039f7f6637503c0286f9c99c224c4ae7814af
altoid <noelreeds01@gmail.com>
2024-12-06 08:51:01 +0000

    NotImplementedError

---
+++ arithmetic-ops/expo.c
@@ -0,0 +1,11 @@
+#include "../arbitrary.h"
+/**
+ * arbitrary_expo - computes factorial of an integer.
+ *
+ *@arr: ptr array of integer to compute.
+ *
+ *Return: returns a factorial of integer on success.
+ */
+uint64_t arbitrary_expo(uint64_t *arr[]) {
+	exit(EXIT_SUCCESS);
+}
---
+++ arithmetic-ops/free.c
@@ -0,0 +1,11 @@
+#include "../arbitrary.h"
+/**
+ * free - free memory in program after use.
+ *
+ *@arr: array of pointers previously allocated memory.
+ *
+ * Return: returns void.
+ */
+void arbitrary_free(void *arr[]) {
+	exit(EXIT_SUCCESS);
+}
