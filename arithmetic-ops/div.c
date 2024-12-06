d313e690dbe44647e75ee642b9389346018ff1c1
altoid <noelreeds01@gmail.com>
2024-12-06 12:11:20 +0000

    handles basic division for real numbers

--- arithmetic-ops/div.c
+++ arithmetic-ops/div.c
@@ -8,8 +8,9 @@
  *
  *Return: result of division.
  */
-uint64_t arbitrary_div(uint64_t num1, uint64_t num2) {
-	uint64_t res;
+long double arbitrary_div(uint64_t num1, uint64_t num2) {
+	uint64_t res, mod_res;
+	long double float_res;
 
 	if (num1 == 0 && num2 == 0) {
 		printf("undefined\n");
@@ -23,13 +24,9 @@ uint64_t arbitrary_div(uint64_t num1, uint64_t num2) {
 		res = 0;
 		return (res);
 	}
-	else if (num2 > num1) {
-		/* NotImplementedErr */
-		exit(1);
-	}
 	else {
-		res = num1 / num2;
-		return (res);
+		float_res = (long double)num1 / (long double)num2;
+		return (float_res);
 	}
 	return (0);
 }
