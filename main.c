d6a426426a02626af922a2e9f5071ee27995a87e
altoid <noelreeds01@gmail.com>
2024-12-06 05:41:28 +0000

    Fixes conflicting data types

--- main.c
+++ main.c
@@ -11,14 +11,13 @@
  */
 
 int main(int argc, char **argv) {
-	int n, m;
-	uint64_t num, res, num3, *arr[argc - 2];
+	int n, i, m = 1;
+	uint64_t num1, num2, res, *arr[argc - 2];
 
 	if (argc < 4) {
 		fprintf(stderr, "Usage: <arithmetic operation> num1 num2.\n");
 		return (0);
 	}
-	m = 1;
 	if (strcmp(argv[m], "add") == 0) {
 		/* check arguments to add func have been
 		* passed to the cmd */
@@ -45,21 +44,22 @@ int main(int argc, char **argv) {
 		res = arbitrary_mul(n, arr);
 		printf("mul: %lu\n", res);
 	}
-	m = 2;
-	num = strtoul(argv[m], NULL, 10);
-	num3 = strtoul(argv[m + 1], NULL, 10);
-
-	if (strcmp(argv[m - 1], "sub") == 0) {
-		res = arbitrary_sub(num, num3);
-		printf("sub: %lu\n", res);
-	}
-	else if (strcmp(argv[m - 1], "div") == 0) {
-		res = arbitrary_div(num, num3);
-		printf("div: %lu\n", res);
-	}
-	else if (strcmp(argv[m - 1], "mod") == 0) {
-		res = arbitrary_mod(num, num3);
-		printf("modulo: %lu\n", res);
+	else {
+		i = 2;
+		num1 = strtoul(argv[i], NULL, 10);
+		num2 = strtoul(argv[i + 1], NULL, 10);
+		if (strcmp(argv[m], "sub") == 0) {
+			res = arbitrary_sub(num1, num2);
+			printf("sub: %lu\n", res);
+		}
+		else if (strcmp(argv[m], "div") == 0) {
+			res = arbitrary_div(num1, num2);
+			printf("div: %lu\n", res);
+		}
+		else if (strcmp(argv[m], "mod") == 0) {
+			res = arbitrary_mod(num1, num2);
+			printf("modulo: %lu\n", res);
+		}
 	}
 	return (0);
 }
