421410ecfe9996dad9ebe7a913c83039f1676b33
altoid <noelreeds01@gmail.com>
2024-12-10 12:33:30 +0000

    computes exponentation

--- arbitrary.h
+++ arbitrary.h
@@ -3,10 +3,13 @@
 
 #define FALSE 0
 #define TRUE 1
+#define LDBL_EXP 2.71828
+
 #define ULL unsigned long long
 
 /*--- header files ---*/
 #include <stdio.h>
+#include <float.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -19,7 +22,7 @@ long double arbitrary_div(uint64_t num1, uint64_t num2);
 uint64_t arbitrary_add(int argc, uint64_t *arr[]);
 int64_t arbitrary_sub(uint64_t num1, uint64_t num2);
 int64_t arbitrary_mul(int argc, uint64_t *arr[]);
-int64_t arbitrary_expo(uint64_t *arr[]);
+long double arbitrary_exp(uint64_t _num);
 void arbitrary_free(void *arr[]);
 uint64_t arbitrary_fact(uint64_t _num);
 
--- arithmetic-ops/expo.c
+++ arithmetic-ops/expo.c
@@ -6,6 +6,18 @@
  *
  *Return: returns a factorial of integer on success.
  */
-int64_t arbitrary_expo(uint64_t *arr[]) {
+long double arbitrary_exp(uint64_t _num) {
+	long double _res = 1;
+
+	if (_num == 0) {
+		return (_res);
+	}
+	else if (_num > 0) {
+		while (_num > 0) {
+			_res = LDBL_EXP * _res;
+			_num--;
+		}
+		return (_res);
+	}
 	exit(EXIT_SUCCESS);
 }
