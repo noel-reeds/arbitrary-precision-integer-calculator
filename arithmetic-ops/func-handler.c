db43e4745b5a88628cc93d9f73ca55122ee461a8
altoid <noelreeds01@gmail.com>
2024-12-10 13:18:49 +0000

    NotImplementedError

---
+++ arithmetic-ops/func-handler.c
@@ -0,0 +1,13 @@
+#include "../arbitrary.h"
+
+/**
+ * func-handler - matches func-name with its implementation.
+ *
+ *@func-name: function string passed to the cmd line.
+ *
+ * Return: returns a matching function if it exists.
+ */
+char func_handler(char *func_name) {
+	/* NotImplementedError */
+	exit(EXIT_SUCCESS);
+}
